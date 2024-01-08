b8d3fbc237e9601b35ddd5f91c68276631522ebb
JJibbz <vkazancev322@gmail.com>
2024-01-08 17:33:45 +0700

    Добавьте файлы проекта.

---
+++ Mod7FinPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mod7FinPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
